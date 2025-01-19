7e4b79bf6de88d6c231dd7636cee8fc5addc1c78
Leandro Coelho <leandrocs1500@gmail.com>
2025-01-19 20:46:36 -0300

    Finalizado

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from flask import Flask, redirect, request
-from peewee import SqliteDatabase
+from flask import Flask
 from routes.home import home_route
 from routes.comentarios import comentarios_route
 from routes.professor import professor_route
-# from database.database import Comentario, Professor
 
 app = Flask(__name__)
-
-#db = SqliteDatabase([Comentarios, Professores])
 
 app.register_blueprint(home_route)
 app.register_blueprint(comentarios_route, url_prefix="/comentarios")
